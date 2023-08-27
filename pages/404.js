6f192fd5516bf3b9dee034c5a17c52f1fbbc8fb4
manigoldTLC <gabrxavier353@gmail.com>
2023-08-27 17:10:31 -0300

    first commit

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function Custom404() {
+    return (
+        <>
+            <h1>404 - Page Not Found</h1>
+            <Link href="/">voltar para a HomePage</Link>
+        </>
+    );
+}
